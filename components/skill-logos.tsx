6eff18074ac697c9a5709f3d8dda9929e85e76a4
Sadikur Rahaman <fahim99369@gmail.com>
2025-10-16 01:53:15 +0600

    Updated Skill logo

--- components/skill-logos.tsx
+++ components/skill-logos.tsx
@@ -186,7 +186,7 @@ export function SkillLogos() {
               </h3>
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 justify-items-center">
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
               {category.skills.map((skill, skillIndex) => (
                 <div
                   key={skillIndex}
